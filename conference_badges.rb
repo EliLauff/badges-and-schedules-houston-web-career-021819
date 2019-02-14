70900364dfdb7bf187bd6b93f639a6ded09b04fc
Eli Lauffenburger <eli.lauffenburger@gmail.com>
2019-02-14 00:26:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  batch = []
+  array.each do |name|
+    batch.push(badge_maker(name))
+  end
+  return batch
+end 
