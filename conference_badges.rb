6892dd0f6858348980c2214580c1285d7a346947
Eli Lauffenburger <eli.lauffenburger@gmail.com>
2019-02-14 00:26:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,10 +3,12 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(speakers)
   batch = []
-  array.each do |name|
+  speakers.each do |name|
     batch.push(badge_maker(name))
   end
   return batch
 end 
+
+def assign_rooms(speakers)
